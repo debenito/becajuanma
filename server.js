f7aa5d0f9c31bcc6fc1425a9a31b6d6e40a0304a
debenito4 <debenito4>
2023-11-21 18:34:19 +0100

    Prueva

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.static('www'));
+app.use(express.static(__dirname));
 app.listen(process.env.PORT || 5000, function(){
     console.log("Node app is running at localhost:" + app.get('port'));
   });
