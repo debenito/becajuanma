0f724b55bc4b09cc105774ddeaf746ff9ab9c63c
debenito4 <debenito4>
2023-11-21 17:03:14 +0100

    PORT

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Node app is running at localhost:" + app.get('port'));
+  });
+
